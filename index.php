2100852b3ebcb1e54909fc54396ffabea972f0ab
Gi@da <giadavolpi8@gmail.com>
2020-01-22 11:19:41 +0100

    grafica

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Google FAQs</title>
+        <link rel="stylesheet" href="public/css/app.css">
+    </head>
+    <body>
+        <?php
+
+        $faqs = ["domanda" => "Come state attuando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) sul diritto all'oblio?", "risposta" => "La recente sentenza della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. Il tribunale ha riscontrato che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere i risultati per le query che includono il nome della persona. Per qualificarsi, i risultati mostrati dovrebbero essere inadeguati, irrilevanti, non più rilevanti o eccessivi.", "domanda" => "In che modo Google protegge la mia privacy e protegge le mie informazioni?", "risposta" => "Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è prioritario fornire una sicurezza elevata e darti la sicurezza che le tue informazioni siano sicure e accessibili quando ne hai bisogno."];
+
+        ?>
+        <header>
+            <div class="header-logo">
+                <img src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-google-sva-scholarship-20.png" alt="logo Google">
+                <span>Privacy & Terms</span>
+            </div>
+            <div class="header-menu">
+                <ul>
+                    <li>Overview</li>
+                    <li>Privacy Policy</li>
+                    <li>Terms of Service</li>
+                    <li>Technologies</li>
+                    <li>FAQ</li>
+                </ul>
+                <span>Google Account</span>
+            </div>
+        </header>
+        <main>
+
+        </main>
+    </body>
+</html>
